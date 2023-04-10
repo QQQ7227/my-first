a7b3517d86477b568c394bacf586db3efe8f1dee
QQQ7227 <Karatel441998@mail.ru>
2023-04-10 17:46:35 +0500

    Задание 2

--- main.js
+++ main.js
@@ -1,4 +1,66 @@
 
-
 console.log("Привет всем!");
 console.log(715);
+
+mybirthDay = '05';
+console.log(mybirthDay);
+console.log('Добро' + 'пожаловать' + 'на курс' + 'Павел');
+// alert('10.04.2023')
+let a = Infinity
+// number
+// let a = Infinity // 1 способ задать явно
+// console.log(1/0);// разделить число на 0
+// NaN
+// console.log('stroka' / 2);
+// bigint
+// console.log(99999999999999999999999999n);
+
+//sring строка
+
+// let str1 = 'JOJO ${str2}'
+// let str2 = "Jojo ${str1}"
+// let str3 = `ANIME ${str2}`
+
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+// boolean булевый
+// let check = true
+// let unCheck = false
+
+// console.log(false == 0);
+
+//null
+// let nuller = null
+// console.log(nuller);
+
+// undefined
+// let a ;
+// console.log(a);
+
+// typeof
+// typeof x
+// typeof(x)
+console.log(typeof 'cat');
+console.log(typeof 2);
+console.log(typeof true);
+console.log(typeof 10n);
+console.log(typeof undefined);
+console.log(typeof null);
+
+document.getElementById('one').innerHTML = 'Привет всем.'
+
+
+
+
+
+
+
+
+
+
+
+
+
+

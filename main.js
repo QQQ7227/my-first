d898eed849871fe7a6a9c4d29b566ae533f6a5d6
QQQ7227 <Karatel441998@mail.ru>
2023-04-11 17:10:08 +0500

    Занятие 3

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ mybirthDay = '05';
 console.log(mybirthDay);
 console.log('Добро' + 'пожаловать' + 'на курс' + 'Павел');
 // alert('10.04.2023')
-let a = Infinity
+// let a = Infinity
 // number
 // let a = Infinity // 1 способ задать явно
 // console.log(1/0);// разделить число на 0
@@ -42,14 +42,39 @@ let a = Infinity
 // typeof
 // typeof x
 // typeof(x)
-console.log(typeof 'cat');
-console.log(typeof 2);
-console.log(typeof true);
-console.log(typeof 10n);
-console.log(typeof undefined);
-console.log(typeof null);
+// console.log(typeof 'cat');
+// console.log(typeof 2);
+// console.log(typeof true);
+// console.log(typeof 10n);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// let inp = document.querySelector('.text')
+// let btn = document.querySelector('.check')
+// let out = document.querySelector('.out')
+// btn.onclick = function() {
+// let e = inp.value
+// out.innerHTML = e
+// inp.value = ''
+// }
 
 document.getElementById('one').innerHTML = 'Привет всем.'
+
+let myday = prompt('Число больше или меньше 100?')
+console.log(myday);
+
+if(myday > 100){
+    console.log(100);
+}
+else{
+    document.getElementById('one').innerHTML = '100';
+}    
+  
+
+    
+
+
+
+
 
 
 

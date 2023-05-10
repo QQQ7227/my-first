ca3bf8a0d4cb7a44546049a539a99aff29d49f8a
QQQ7227 <Karatel441998@mail.ru>
2023-05-10 22:43:55 +0500

    Занятие 10

--- main.js
+++ main.js
@@ -1,141 +1,12 @@
-
-// console.log("Привет всем!");
-// console.log(715);
-
-// mybirthDay = '05';
-// console.log(mybirthDay);
-// console.log('Добро' + 'пожаловать' + 'на курс' + 'Павел');
-// alert('10.04.2023')
-// let a = Infinity
-// number
-// let a = Infinity // 1 способ задать явно
-// console.log(1/0);// разделить число на 0
-// NaN
-// console.log('stroka' / 2);
-// bigint
-// console.log(99999999999999999999999999n);
-
-//sring строка
-
-// let str1 = 'JOJO ${str2}'
-// let str2 = "Jojo ${str1}"
-// let str3 = `ANIME ${str2}`
-
-// console.log(str1);
-// console.log(str2);
-// console.log(str3);
-
-// boolean булевый
-// let check = true
-// let unCheck = false
-
-// console.log(false == 0);
-
-//null
-// let nuller = null
-// console.log(nuller);
-
-// undefined
-// let a ;
-// console.log(a);
-
-// typeof
-// typeof x
-// typeof(x)
-// console.log(typeof 'cat');
-// console.log(typeof 2);
-// console.log(typeof true);
-// console.log(typeof 10n);
-// console.log(typeof undefined);
-// console.log(typeof null);
-// let inp = document.querySelector('.text')
-// let btn = document.querySelector('.check')
-// let out = document.querySelector('.out')
-// btn.onclick = function() {
-// let e = inp.value
-// out.innerHTML = e
-// inp.value = ''
-// }
+// Домашняя работа: Занятие 2.
 
 // document.getElementById('one').innerHTML = 'Привет всем.'
 
+// Домашняя работа: Занятие 3.
 // let myday = prompt('Число больше или меньше 100?')
 // console.log(myday);
 
-// if(myday > 100){
-//     console.log(100);
-// }
-// else{
-//     document.getElementById('one').innerHTML = '100';
-// } 
-
-// let year = prompt('В каком году вышел ходячий замок?')
-// // не верная запись
-// if(year <2004){
-// console.log('Рано');
-// }
-// else if(year>2004){
-// console.log('Поздно');
-// }
-// else{
-// console.log('Верно');
-// }
-// // не совсем верная запись
-// if(year <2004){
-// console.log('Рано');
-// }
-// if(year>2004){
-// console.log('Поздно');
-// }
-// if(year ==2004){
-// console.log('Верно');
-// }
-
-
-// let day = prompt('Сколько днй в неделе?')
-// let week = (day == 7) ? 'Отлично' : 'Ты не умеешь считать'
-// // let Результат = Условие ? если True Первый вариант : иначе Второй вариант
-// // if(day== 7){
-// // week =true
-// // }
-// // else{
-// // week = false
-// // }
-// console.log(week);
-
-// ВОзведение в степень **
-
-// console.log(2**2);
-
-// Оператор остатка от деления %
-
-// console.log(10%2.5);
-
-//< > - оператор больше или меньше
-//>= <= - Больше или равно меньше или равно
-//== - Не строгое равно
-// === - CТрогое равно
-// = - оператор присваивания
-// != - не равно
-
-// ||
-// true || true = true
-// false || true = true
-// true || false = true
-// false || false = false
-
-// let a =4
-// let b = 10
-// if(a<5 || a>5){
-// console.log(b);
-// }
- 
-
-// &&
-// true || true = true
-// false || true = false
-// true || false = false
-// false || false = false
+// Домашняя работа: Занятие 4.
 
 // let a = 5
 // let b = 10
@@ -150,7 +21,23 @@
 //     console.log('Нет');
 // }
 
+// Домашняя работа: Занятие 5.
+// for(let i =1;i<=50;i++){
+//     console.log(i);
+// }
 
+// for(let i =50;i>24;i--){
+//     console.log(i);
+// }
+
+// let i = 2500;
+// while(i<3000){
+//   i++;
+//   if(i % 2)
+//    console.log(i);
+// }
+
+// Домашняя работа: Задание 6.
 // let a = 2;
 // let b = 2;
 
@@ -163,125 +50,6 @@
 
 // let result = (a === b)
 //     console.log(result);
-
-
-
-// let i = 0;
-
-// while(i<3){
-// // alert(i)
-// i++
-// }
-// 1 итерация i = 0
-// 2 итерация i = 1
-// 3 итерация i = 2
-// 4 итерация i = 3
-
-// let i = 0;
-// // i++
-// // ++i
-// console.log(++i);    
-
-// ключевое слово do{
-//     тело цикла
-//     } ключевое слово while(условие)
-
-// for(let i = 0; i < 20;i+=2){
-//     console.log(i);
-//     }
-
-
-//     for(let i = 0; i < 3;i++){
-//         console.log(i);
-//         }
-        
-//         // (Если условие == true -> Выполнить тело, Выполнить шаг)
-//         // (Если условие == true -> Выполнить тело, Выполнить шаг)
-//         // (Если условие == true -> Выполнить тело, Выполнить шаг)
-//         // ......
-        
-//         // выполняем начало
-//         let i = 0;
-//         // Если условие == true -> Выполнить тело, Выполнить шаг
-//         if(i<3){console.log(i)};i++
-//         // Если условие == true -> Выполнить тело, Выполнить шаг
-//         if(i<3){console.log(i)};i++
-//         // Если условие == true -> Выполнить тело, Выполнить шаг
-//         if(i<3){console.log(i)};i++
-//         // Если условие == true -> Выполнить тело, Выполнить шаг
-//         // конец цикла
-
-
-//         let i = 0
-//         for(i = 0; i < 3;i++){
-//         console.log(i);
-//         }
-//         console.log(i);
-
-
-//         let i = 0
-//         for(;;){
-//         console.log(i++);
-//         if(i>10) break;
-//         console.log(i+10);
-//         }
-
-
-//         for(let i =0; i<10; i++){
-//             if(i % 2 ==0 ){
-//             console.log(i);
-//             }
-            
-//             }
-
-
-// for(let i =1;i<=50;i++){
-//     console.log(i);
-// }
-
-
-// for(let i =50;i>24;i--){
-//     console.log(i);
-// }
-
-
-// let i = 2500;
-// while(i<3000){
-//   i++;
-//   if(i % 2)
-//    console.log(i);
-// }
-
-
-// let a = 5;
-// switch(a){
-// case 12:
-// alert('Правильно');
-// break
-
-// case 5:
-// case 10:
-// alert(1)
-// break
-// alert(2)
-// break
-// default:
-// alert('результат кривой однознанчно')
-
-// }
-
-// let a = '1';
-// let b = 0;
-// switch(+a){
-// case b + 1:
-// console.log('Выполнится потому как значением +а, будет в точности равно и + 1');
-// break
-// default:
-// console.log('Это не выполнится ');
-// }
-// let c = + a
-// console.log();
-
 
 // let a = 2 + 2;
 
@@ -315,77 +83,94 @@
 //     alert('осень');
 //     break;
 // }
+// Домашняя работа: Задание 7.
 
+    // let menu = prompt("Калькулятор: 1) сложение, 2) вычитание, 3)умножение, 4) деление")
+    // let qwert = document.querySelector('.qwert')
+    // function calc1(a, b) {
+    //     if (menu == 1){
+    //     let s1 = prompt("a");
+    //     let s2 = prompt("b");
+    //     let sum = +s1 + +s2;
+    //     qwert.innerHTML = sum;
+    //     }
+    //     }
+    //     function calc2(a, b) {
+    //     if (menu ==2){
+    //     let s1 = prompt("a");
+    //     let s2 = prompt("b");
+    //     let sub = s1 - s2;
+    //     qwert.innerHTML =sub;
+    //     }
+    //     }
+    //     function calc3(a, b) {
+    //     if (menu == 3){
+    //     let s1 = prompt("a");
+    //     let s2 = prompt("b");
+    //     let multi = s1 * s2;
+    //     qwert.innerHTML = multi;
+    //     }
+    //     }
+    //     function calc4(a, b) {
+    //     if (menu == 4){
+    //     let s1 = prompt("a");
+    //     let s2 = prompt("b");
+    //     let div = s1 / s2;
+    //     qwert.innerHTML =div;
+    //     }
+    //     }
+        
+    //     calc1()
+    //     calc2()
+    //     calc3()
+    //     calc4()
 
-
-// let= null
-// let= 'Пока'
-// let= 'Привет'
-// alert(null ?? 'Пока' ?? 'Привет')
-
-
-// function decloration
-//
-// ключевое слово function название функции(параметры){
-//     тело функции
-//     }
-//     function one(){
-//     console.log('Наша первая осознанная функция');
-//     }
-
-// let i = 0
-// function one(){
-// console.log('Наша первая осознанная функция');
-// alert('Ты нажал кнопку ' + i + ' раз')
-// i+=1
-// }
-
-// let btn = document.querySelector('.check')
-
-// btn.onclick = one
-
-// function borsh(){
-//     console.log('порезать лук ');
-//     console.log('овощи');
-//     console.log('поставить кастрюлю ');
-    
-//     }
-//     borsh()
-
-// let a = 12
-// let b = 118
-
-// function multiply(){
-// return a + b
-
-// }
-// multiply()
-
-// let a = prompt('Ваше имя?')
-// function mlt(x,y){
-// return x + y
-// }
-// let c = mlt('Добро пожаловать ', a )
-// console.log(c);
-
-    // const one = document.querySelector('.one')
-    // one.onclick = function (){  
-    // alert=2
-    // }
-
-    // document.button.innerHTML = (one.onclick= function(){alert=2})
-
+    // Домашняя работа: Задание 8.
 
     // const one = document.querySelector('.one')
     // one.onclick = function (){
     // Element.style.widthHeight = '300px'
     // }   
 
-    const one = document.querySelector('.one')
-    one.onclick = function (){
+    // const one = document.querySelector('.one')
+    // one.onclick = function (){
 
 
 
 
-    Mydiv1.classlist.remove('Mydiv1')
-    }
+    // Mydiv1.classlist.remove('Mydiv1')
+    // }
+
+
+    // Домашняя работа: Задание 9.
+    // let one = document.querySelector('.one');
+    // let Mydiv1 = document.querySelector('.Mydiv1');
+
+
+
+    // let a=[0,1,2,3,4,5,6,7,8,9,10,11,12];
+    // one.onclick = a.splice(4,7)  
+    // if (i < a.length){
+    // Mydiv1.innerHTML += a[i++] + "<br />"};
+
+    // Домашняя работа: Задание 10.
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=57.15&lon=65.51&appid=7ac3da5e7f1876686f8c4dbe01b8d452')
+    .then(function(resp){return resp.json()})
+    .then(function(data){
+        document.querySelector('.wind').textContent = data.wind.speed
+        document.querySelector('.humidity').textContent = data.main.humidity
+        document.querySelector('.country').innerHTML = data.sys.country.
+        document.querySelector('.sunrise').textContent = data.sys.sunrise.
+        document.querySelector('.sunset').textContent = data.sys.sunset.
+        console.log(data);
+    })
+
+
+    
+
+     
+     
+    
+    
+
